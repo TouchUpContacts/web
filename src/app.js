633edfbd9922248bbbe3ff189b819bcd3d0667c8
Laurent Janet <laurent.janet@gmail.com>
2017-04-02 15:54:43 +0200

    implements navigation item active modifier mapper

--- src/app.js
+++ src/app.js
@@ -6,11 +6,17 @@
 
   touchUpApp.slideContainer = document.body;
   touchUpApp.navItems = Array.from(document.querySelectorAll('[data-step]'));
+  touchUpApp.activeItem = touchUpApp.navItems[0];
 
   itemClickEventHandler = (e) => {
-    let itemDataset = e.currentTarget.dataset;
+    let item = e.currentTarget;
+    let itemDataset = item.dataset;
     let slideContainer = touchUpApp.slideContainer;
     let slideContainerModifierClass = `slide-container--${itemDataset.step}`;
+
+    touchUpApp.activeItem.classList.remove('item--active');
+    item.classList.add('item--active');
+    touchUpApp.activeItem = item;
 
     slideContainer.className = slideContainer.className.replace(slideContainerModifierRegExp, slideContainerModifierClass);
   }
