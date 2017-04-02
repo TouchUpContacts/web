b2a6157b3ae836a960eeb0ba2a2abba10bc3cef7
Laurent Janet <laurent.janet@gmail.com>
2017-04-02 15:43:03 +0200

    implements body background-color transition

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
 (function () {
   "use strict";
+
+  let touchUpApp = {}, itemClickEventHandler;
+  const slideContainerModifierRegExp = /slide-container--[a-z\-]+/g;
+
+  touchUpApp.slideContainer = document.body;
+  touchUpApp.navItems = Array.from(document.querySelectorAll('[data-step]'));
+
+  itemClickEventHandler = (e) => {
+    let itemDataset = e.currentTarget.dataset;
+    let slideContainer = touchUpApp.slideContainer;
+    let slideContainerModifierClass = `slide-container--${itemDataset.step}`;
+
+    if(!slideContainerModifierRegExp.test(slideContainer.className)) {
+      slideContainer.classList.add(slideContainerModifierClass);
+    } else {
+      slideContainer.className = slideContainer.className.replace(slideContainerModifierRegExp, slideContainerModifierClass);
+    }
+  }
+
+  touchUpApp.navItems.forEach((item) => {
+    item.addEventListener('click', itemClickEventHandler);
+  });
+
+  window.TouchUpApp = window.TouchUpApp || Object.create(touchUpApp);
 }());
